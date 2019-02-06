9bab13b9e4b8438a53ea0b06799176144b14b3e7
stepanoves <36924394+stepanoves@users.noreply.github.com>
2019-02-06 16:44:12 +0300

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    const operation = document.getElementById('operation');
-    const writerType = document.getElementById('writer-type');
-    const doButton = document.getElementById('do');
-    doButton.addEventListener('click', () => {
-        WriterExecutor.execute(writerType.value, CommandExecutor.execute(commandParser(operation.value)), []);
-    });
-})();  
