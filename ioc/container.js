40f0df33c3bce5e79e72fe9149b06fb577248b66
stepanoves <stepanovyahor@gmail.com>
2019-02-07 12:17:03 +0300

    ioc example add setDependencies

--- ioc/container.js
+++ ioc/container.js
@@ -7,6 +7,11 @@ class Container {
         this.__map.set(key, {className, dependenciesArray});
     }
 
+    setDependencies(key, dependenciesArray) {
+        this.__map.get(key).dependenciesArray = dependenciesArray;
+        console.log(this.__map.get(key).dependenciesArray)
+    }
+
     getDependencies(key) {
         if (this.__map.get(key).dependenciesArray !== undefined) {
             return this.__map.get(key).dependenciesArray.map((el) => {
@@ -45,9 +50,24 @@ class Wheel {
     }
 }
 
+class Wheel2 {
+    constructor() {
+        this.__name = 'wheel2'
+    }
+
+    getName() {
+        return this.__name;
+    }
+}
+
 const cntr = new Container();
 cntr.register('wheel', Wheel);
+cntr.register('wheel2', Wheel2);
 cntr.register('car', Car, ['wheel']);
 
 var car = cntr.get('car');
+console.log(car.getWheel())
+
+cntr.setDependencies('car', ['wheel2']);
+car = cntr.get('car');
 console.log(car.getWheel())
